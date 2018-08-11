91387782bb1d3567de905eb96359a57144bd1222
BRTOKEN\leandro.castilho <leandro.castilho@datablink.com>
2018-08-10 23:23:36 -0300

    portifolio

---
+++ Portifolio/js/app.js
@@ -0,0 +1,38 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#main-nav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#main-nav").offset().top > 100) {
+      $("#main-nav").addClass("navbar-shrink");
+    } else {
+      $("#main-nav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
